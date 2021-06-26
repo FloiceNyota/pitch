957ac0da086e310ed16dce95b70d6216ecc53003
FloiceNyota98 <nyotafloice@gmail.com>
2021-06-26 12:54:35 +0300

    initial commit

---
+++ instance/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY='dagcyrus'
+MAIL_USERNAME='nyotafloice@gmail.com'
+MAIL_PASSWORD='0716501927'
---
+++ start.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY='dagcyrus'
+export MAIL_USERNAME='nyotafloice@gmail.com'
+export MAIL_PASSWORD='0716501927'
+python3.8 manage.py server
